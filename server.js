873e5a5ed4093e268ea5ee6a5ccad09cab3a1a31
sg-guilherme.ignacio <guilherme.ignacio@softgames.de>
2020-07-17 14:27:28 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 8080
+const recipeFilter = require('./src/recipeFilter')
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/recipe', (req, res) => {
+    console.log(req.body.ingredients)
+    const bestRecipe = recipeFilter(req.body.ingredients);
+    res.send({bestRecipe});
+})
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
